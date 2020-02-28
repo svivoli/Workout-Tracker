8869f7834c9af80f655ab377944521b69d7504fe
Sarah Vivoli <snvivoli@gmail.com>
2020-02-28 10:13:42 -0800

    html routes

--- app.js
+++ app.js
@@ -18,3 +18,15 @@ db.on("error", error => {
     console.log("Database Error: ", error);
 });
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/exercise.html'));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/stats.html'));
+});
+
