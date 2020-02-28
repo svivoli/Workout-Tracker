ba15cda7199b355e2f749f19a6c2612af6d77edd
Sarah Vivoli <snvivoli@gmail.com>
2020-02-28 09:52:53 -0800

    app use

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const mongojs = require('mongojs');
 const path = require('path');
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
