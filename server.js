38aeb8d5acf7028ed55ec383df64250f8485b7e5
Sarah Vivoli <snvivoli@gmail.com>
2020-02-28 09:57:34 -0800

    database setup in server.js

--- server.js
+++ server.js
@@ -8,3 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+
+const databaseURL = "workout-tracker";
+const collections = "workouts";
+
+const db = mongojs(databaseURL, collections);
+
+db.on("error", error => {
+    console.log("Database Error: ", error);
+});
+
