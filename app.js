c9eec0c1e2e1938e7ad80672e9a055da02c3a481
Sarah Vivoli <snvivoli@gmail.com>
2020-02-28 12:40:26 -0800

    api/workouts post route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 
 const PORT = process.env.PORT || 8080;
@@ -10,7 +11,10 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", { useNewUrlParser: true });
+
 require('./routes/html-routes.js')(app);
+require('./routes/api-routes.js')(app);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,15 @@
+const Workout = require('../models/workout');
 
+module.exports = function (app) {
+    app.post('/api/workouts', (req, res) => {
+        console.log(body);
+        Workout.create({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+};
 
